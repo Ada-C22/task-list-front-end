806dcbe8aee2ba37686dd354fa37494747b248fc
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-12-18 11:37:25 -0600

    Update the toggle complete feature of each Task to update the state of the task data stored in App

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, onToggleComplete }) => {
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={() => onToggleComplete(id)}
       >
         {title}
       </button>
@@ -24,6 +21,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  onToggleComplete: PropTypes.func.isRequired,
 };
 
 export default Task;
