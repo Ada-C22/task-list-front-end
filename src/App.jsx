f974541f4b0e87e5078cc7a10663f83c7d9bb215
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-01-06 09:09:54 -0600

    .env update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
-  const API_BASE_URL = 'http://127.0.0.1:5000';
+  const API_BASE_URL = import.meta.env.API_BASE_URL;
 
   useEffect(() => {
     axios
